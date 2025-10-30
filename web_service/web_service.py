edeb6b0834564cdff0209cb45d31f3e5c10a61cc
elanuroguc-blip <elanuroguc@gmail.com>
2025-10-30 14:51:17 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_URL =https://hello-cloud3-3-kkdd.onrender.com/ziyaretçiler
+API_URL = "https://hello-cloud3-3-kkdd.onrender.com/ziyaretçiler"
 
 HTML = """
 <!doctype html>

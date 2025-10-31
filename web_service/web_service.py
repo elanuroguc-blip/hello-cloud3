92daed393e799122211e7c3d2b8938d00e5dbcdd
elanuroguc-blip <elanuroguc@gmail.com>
2025-10-31 11:51:03 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -8,32 +8,34 @@ API_URL = "https://hello-cloud3-13.onrender.com"
 HTML = """
 <!doctype html>
 <html>
-<head>
-<title>Mikro Hizmetli Selam!</title>
-<style>
-body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
-h1 { color: #333; }
-input { padding: 10px; font-size: 16px; }
-button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
-li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
-</style>
-</head>
-<body>
-<h1>Mikro Hizmetli Selam!</h1>
-<p>Adını yaz</p>
-<form method="POST">
-<input type="text" name="isim" placeholder="Adını yaz" required>
-<input type="text" name="sehir" placeholder="Şehrini yaz" required>
-<button type="submit">Gönder</button>
-</form>
-<h3>Ziyaretçiler:</h3>
-<h3>Şehirler:</h3>
-<ul>
-{% for ad in isimler %}
-<li>{{ ad }}</li>
-{% endfor %}
-</ul>
-</body>
+  <head>
+    <title>Mikro Hizmetli Selam!</title>
+    <style>
+      body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
+      h1 { color: #333; }
+      input { padding: 10px; font-size: 16px; margin: 5px; }
+      button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
+      li { background: white; margin: 5px auto; padding: 10px; border-radius: 5px; max-width: 350px; text-align: left; }
+      ul { list-style-type: none; padding: 0; }
+      form { margin-bottom: 20px; }
+    </style>
+  </head>
+  <body>
+    <h1>Mikro Hizmetli Selam!</h1>
+    
+    <form method="POST">
+      <input type="text" name="isim" placeholder="Adını yaz" required>
+      <input type="text" name="sehir" placeholder="Şehrini yaz" required>
+      <button type="submit">Gönder</button>
+    </form>
+    
+    <h3>Ziyaretçiler ve Şehirleri:</h3>
+    <ul>
+      {% for ziyaretci in isimler %}
+        <li>{{ ziyaretci.isim }} - ({{ ziyaretci.sehir }})</li>
+      {% endfor %}
+    </ul>
+    </body>
 </html>
 """
 

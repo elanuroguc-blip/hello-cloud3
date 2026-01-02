978c67358e39cceb16a2b171ee3d7af8fc86ad2e
elanuroguc-blip <elanuroguc@gmail.com>
2026-01-02 20:09:11 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -10,7 +10,7 @@ HTML = """
 <!doctype html>
 <html>
 <head>
-    <title>Ziyaretçi Defteri</title>
+    <title>Elanur Ögüç</title>
     <style>
         body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
         h1 { color: #333; }
